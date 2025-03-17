7e19ea0913d2a28872cdb75dcdc8ec5a7c573664
Yash Raj <kyashraj991@gmail.com>
2025-03-17 22:23:09 +0530

    server test

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,2 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 3001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('HELLO FROM SERVER');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
