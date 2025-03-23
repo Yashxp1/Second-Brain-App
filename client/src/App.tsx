3459dc630debe39112bf40da5a76ce9ef4828634
Yash Raj <kyashraj991@gmail.com>
2025-03-23 16:40:01 +0530

    Icons steup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,12 @@
-import { Home } from './pages/Home';
-import { Route, Routes } from 'react-router-dom';
-import Signin  from './pages/Signin';
-import Signup  from './pages/Signup';
-import Dashboard  from './pages/Dashboard';
+import { Button } from './components/Button';
+import PlusIcon from './Icons/PlusIcon';
+import ShareIcon from './Icons/ShareIcon';
 
 const App = () => {
   return (
-    <div className="">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Signin/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/signin" element={<Signin/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-      </Routes>
+    <div>
+      <Button variant="primary" text="Add content" startIcon={<PlusIcon />} />
+      <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon />} />
     </div>
   );
 };
