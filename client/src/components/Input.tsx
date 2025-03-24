92efa79e8bc6bb114da0b33ec84ff70d4de27491
Yash Raj <kyashraj991@gmail.com>
2025-03-24 22:32:17 +0530

    BACKEND API CONFIG

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,16 +1,17 @@
-export function Input({
-  onChange,
-  placeholder,
-}: {
+interface InputProps {
   placeholder: string;
-  onChange: () => void;
-}) {
+  ref?: any;
+}
+
+export function Input({ placeholder, ref }: InputProps) {
   return (
     <div>
       <input
+        ref={ref}
         placeholder={placeholder}
+        type={"text"}
         className="px-4 py-2 border rounded m-2"
-        onChange={onChange}
+        
       />
     </div>
   );
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,14 +1,23 @@
 import { Button } from '../components/Button';
 import { Input } from '../components/Input';
+import { useRef } from 'react';
 
 export function Signup() {
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  function signup() {
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+  }
+
   return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
       <div className="bg-white border min-w-48 p-8 rounded-xl">
-        <Input placeholder="Username" />
-        <Input placeholder="Password" />
+        <Input ref={usernameRef} placeholder="Username" />
+        <Input ref={passwordRef} placeholder="Password" />
         <div className="flex justify-center pt-4">
-          <Button loading={false} variant="primary" text="Signup" fullwidth={true} />
+          <Button onClick={signup} loading={false} variant="primary" text="Signup" fullwidth={true} />
         </div>
       </div>
     </div>
