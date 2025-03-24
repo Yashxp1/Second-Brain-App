a39813bdbc473111d0da9df899b8d5ab0c75072b
Yash Raj <kyashraj991@gmail.com>
2025-03-24 21:55:35 +0530

    Routes setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import Dashboard from './pages/Dashboard';
 import { Signup } from './pages/Signup';
 import { Signin } from './pages/Signin';
+import { Home } from './pages/Home';
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      {/* <Signup /> */}
-      <Signin />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 };
 
